ba0e534d2900ff317bb0051e8ddb39b819ecb13d
Julien Le Mee <julemee@gmail.com>
2022-11-07 17:16:24 -0500

    FSC reboot - rails longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = []
@@ -5,6 +7,21 @@ class GamesController < ApplicationController
   end
 
   def score
-    @word = params[:word]
+    @word = (params[:word] || '').upcase
+    @letters = params[:letters].split
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'games/new', to: 'games#new'
-  post 'games/score', to: 'games#score'
+  get 'new', to: 'games#new'
+  post 'score', to: 'games#score'
 end
