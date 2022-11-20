c382b4847b75f31c8747a297c01713650456c87e
Julien Le Mee <julemee@gmail.com>
2022-11-20 16:21:38 -0500

    added voyelles array

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,22 @@
 require 'open-uri'
 
 class GamesController < ApplicationController
+  VOWELS = ['A','E','I','O','U','Y']
+
   def new
-    @letters = []
-    10.times { @letters << ('A'..'Z').to_a.sample }
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
+  # def new
+  #   @letters = []
+  #   10.times { @letters << ('A'..'Z').to_a.sample }
+  # end
+
   def score
-    @word = (params[:word] || '').upcase
     @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
     @included = included?(@word, @letters)
     @english_word = english_word?(@word)
   end
