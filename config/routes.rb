0c989cb084d969cb193735a92ccdb2e16e6d7436
Julien Le Mee <julemee@gmail.com>
2022-11-20 16:08:45 -0500

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'games#new'
+
   get 'new', to: 'games#new'
   post 'score', to: 'games#score'
 end
